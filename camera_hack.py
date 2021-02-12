34480d1ba9b4646ceea8da5ff80adef0e7a011ea
khalilhacker2231 <78014267+khalilhacker2231@users.noreply.github.com>
2021-02-12 23:24:00 +0430

    Update camera_hack.py

--- camera_hack.py
+++ camera_hack.py
@@ -39,7 +39,7 @@ print("""
 try:
     num = int(input("OPTIONS : "))
 
- if num == 1: 
+ elif num == 1:
         print("\n")
         try:
               headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux i686; rv:68.0) Gecko/20100101 Firefox/68.0'}
